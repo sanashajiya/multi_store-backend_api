f14c6c9689917432d70df11a47062deabe8d6c93
sanashajiya <ssanashajiya@gmail.com>
2025-05-13 17:01:08 +0530

    Get Customer Order

--- routes/order.js
+++ routes/order.js
@@ -42,4 +42,23 @@ orderRouter.post('/api/orders', async (req, res) => {
         res.status(500).json({error: e.message});
     }
 });
+// GET route for fetching orders by buyerID
+orderRouter.get('/api/orders/:buyerId', async (req, res) => {
+    try {
+        // extract the buyerId from the request parameters
+        const {buyerId} = req.params;
+        // find all orders in the database that match the buyerId
+        const orders = await Order.find({buyerId});
+        // if no orders are found, return 404 page not found status with an error message
+        if(orders.length == 0){
+            return res.status(404).json({msg: "No orders found for this buyer!"});
+        }
+        // if orders are found, return 200 status with the orders
+        return res.status(200).json(orders);
+    } catch (e) {
+        // handle any errors that occur during the order retrieval process
+        res.status(500).json({error: e.message});
+    }
+});
+
 module.exports = orderRouter;
